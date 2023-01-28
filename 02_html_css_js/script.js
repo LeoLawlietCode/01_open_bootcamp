8220cb7dae31d9ce80c8884361a82e907d721965
Leonardo Caicedo <leonardoivan97@hotmail.com>
2023-01-28 18:11:53 -0500

    Ejercicio 2

--- 02_html_css_js/script.js
+++ 02_html_css_js/script.js
@@ -1,15 +1,12 @@
-const print = (algo) => {
-  console.log(algo)
-}
-
 const calcularValorMensual = () => {
   var cantidad = document.getElementById("cantidad").value;
   var cuotas = document.getElementById("cuotas").value;
   var tipo = document.getElementById("tipo").value;
 
   interes = (tipo / 12) / 100
+  // Cálculo de acuerdo a la fórmula V = C * (((1 + i)^n)*i / ((1 + i)^n) - 1))
   resutlado = cantidad * (Math.pow((1 + interes), cuotas) * interes) / (Math.pow((1 + interes), cuotas) - 1)
-  print(resutlado.toFixed(2))
 
+  // Edición de la etiqueta <p> para mostrar el valor
   document.getElementById("resultado").innerHTML = "€ " + resutlado.toFixed(2)
 }
